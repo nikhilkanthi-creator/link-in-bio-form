fa4ae38b4fc12895af91c0f7e8036acf93601633
Nikhil Kanthi <nkanthi@chicagobooth.edu>
2025-05-06 07:12:29 +0000

    mostly done

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,6 +10,13 @@ class ItemsController < ApplicationController
   end
 
   def create_item
+    # the params hash looks like: {"link_url_param"=>"test", "link_description_param"=>"test", "thumbnail_url_param"=>"1"}
+    i = Item.new
+    i.link_url = params.fetch("link_url_param")
+    i.link_description = params.fetch("link_description_param")
+    i.thumbnail_url = params.fetch("thumbnail_url_param")
+    i.save
+    
   render({ :template => "item_templates/create"})
   end
 
